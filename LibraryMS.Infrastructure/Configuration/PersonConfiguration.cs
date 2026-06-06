b071acf530e651d9c0c042ef2bbe8fec5ee619b1
iissa-dev <issasn99@gmail.com>
2026-06-06 18:17:39 +0300

    feat(person): implement People Table

---
+++ LibraryMS.Infrastructure/Configuration/PersonConfiguration.cs
@@ -0,0 +1,36 @@
+namespace LibraryMS.Infrastructure.Configuration;
+
+public class PersonConfiguration : IEntityTypeConfiguration<Person>
+{
+    public void Configure(EntityTypeBuilder<Person> builder)
+    {
+        builder.ToTable("People");
+        builder.HasKey(p => p.Id);
+
+        builder.HasOne(u => u.Country)
+            .WithMany()
+            .HasForeignKey(p => p.CountryId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Property(p => p.FirstName)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(p => p.LastName)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(p => p.Address)
+            .HasMaxLength(500)
+            .IsRequired();
+
+        builder.Property(p => p.DateOfBirth)
+            .HasColumnType("date")
+            .IsRequired();
+
+        builder.Property(p => p.ImageUrl)
+            .HasColumnType("nvarchar(500)")
+            .HasMaxLength(500)
+            .IsRequired(false);
+    }
+}
